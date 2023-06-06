19f76d00c85765334f9ad112b1cb98aadf6677c8
Nicholas Mayne <nick@modelrocket.io>
2023-06-06 15:07:10 -0700

    check in infrastructure

---
+++ bunny-worker-locations.py
@@ -0,0 +1,18 @@
+import sys
+import requests
+import json
+import logging
+import time
+
+logging.captureWarnings(True)
+
+def solution(x, y):
+    # Your solution here 
+    return str(0)
+
+def main():
+    print(solution(5, 10))
+    print(solution(3, 2))
+
+if __name__ == "__main__":
+    main()
