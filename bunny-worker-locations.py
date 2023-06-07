095f9c8ed9076a48176709d55a62653cd666c28b
Nicholas Mayne <nmayne@gmail.com>
2023-06-07 00:27:24 -0700

    finish first one

--- bunny-worker-locations.py
+++ bunny-worker-locations.py
@@ -8,11 +8,24 @@ logging.captureWarnings(True)
 
 def solution(x, y):
     # Your solution here 
-    return str(0)
+    value = 1
+    delta = 2
+    for i in range(1, x):
+        value += delta
+        delta += 1
+    
+    delta = x
+    for k in range(1, y):
+        value += delta
+        delta += 1
+
+    return str(value)
 
 def main():
     print(solution(5, 10))
     print(solution(3, 2))
+    print(solution(3, 20000))
+    print(solution(1000, 2))
 
 if __name__ == "__main__":
     main()
