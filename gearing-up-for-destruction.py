fd1b103e7155d359d483541a03de85042e6f9275
Nicholas Mayne <nmayne@gmail.com>
2023-06-07 00:29:09 -0700

    initial checkin

---
+++ gearing-up-for-destruction.py
@@ -0,0 +1,17 @@
+import sys
+import requests
+import json
+import logging
+import time
+
+logging.captureWarnings(True)
+
+def solution(pegs):
+    pass
+
+def main():
+    print(solution([4, 17, 50]))
+    print(solution([4, 30, 50]))
+
+if __name__ == "__main__":
+    main()
